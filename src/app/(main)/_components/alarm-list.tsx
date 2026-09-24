15764ce387b0edd1cb81c36ce3b627f360fd3b71
Juan David Castañeda <jcastanedaaguirre@gmail.com>
2026-09-24 17:22:39 -0500

    feat: add alarms list to main page

---
+++ src/app/(main)/_components/alarm-list.tsx
@@ -0,0 +1,35 @@
+type Alarm = {
+  id: number;
+  name: string;
+  time: string;
+  active: boolean;
+};
+
+const initialAlarms: Alarm[] = [
+  { id: 1, name: "Mi alarma madrugadora", time: "05:30", active: true },
+  { id: 2, name: "Trabajo", time: "08:00", active: true },
+  { id: 3, name: "Siesta", time: "15:30", active: false },
+];
+
+export function AlarmList() {
+  const alarms = initialAlarms;
+
+  return (
+    <section className="flex flex-col gap-4 min-w-75 flex-[1_1_25%]">
+      <header className="flex items-center justify-between">
+        <h1 className="font-bold text-[22px]">Tus alarmas</h1>
+      </header>
+
+      {alarms.map(({ id, name, time }) => (
+        <article
+          key={id}
+          className="flex flex-col gap-2 border border-solid rounded-[14px] p-4.5 bg-white border-hard-gray"
+        >
+          <h2 className="text-[13px] font-bold">{name}</h2>
+          <hr className="border-hard-gray w-24" />
+          <span className="font-bold text-[28px]">{time}</span>
+        </article>
+      ))}
+    </section>
+  );
+}
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,3 +1,9 @@
+import { AlarmList } from "./_components/alarm-list";
+
 export default function MainPage() {
-  return <p>Main page</p>
+  return (
+    <main className="bg-[#f3f3f0] flex-1 w-full py-4.75 px-8 flex gap-6 flex-wrap items-start">
+      <AlarmList />
+    </main>
+  );
 }
